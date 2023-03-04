b076c45bcde58a0f72dfd386a6088c398e286b7b
Dmitry <dkrukovskiy@yandex.ru>
2023-03-05 00:14:05 +0400

    finished

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,100 +3,24 @@ import configuration
 import data
 
 
+#  Функция отправляет POST-запрос на создание нового пользователя.
 def post_new_user(user_body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
                          json=user_body,
                          headers=data.headers)
 
 
-#  response = post_new_user(data.user_body)
-#  print(response.status_code)
-#  print(response.text)
-#  print(response.json()['authToken'])
-
-
+#  Функция возвращает токен авторизации из POST-запроса на создание нового пользователя.
 def get_new_user_token():
     response = post_new_user(data.user_body)
-#    auth_token = response.json()['authToken']
     return response.json()['authToken']
 
 
-def post_new_client_kit(kit_body, client_auth_token):
-    headers = {
-        "Authorization": "Bearer " + client_auth_token,
-        "Content-Type": "application/json",
-    }
+#   Функция отправляет POST-запрос на создание нового набора и использует словарь с данными набора (kit_body)
+#   и токен авторизации (auth_token).
+def post_new_client_kit(kit_body, auth_token):
+    headers_dict = data.headers.copy()
+    headers_dict["Authorization"] = "Bearer " + auth_token
     url = configuration.URL_SERVICE + configuration.CREATE_KITS_PATH
-    response = requests.post(url, json=kit_body, headers=headers)
+    response = requests.post(url, json=kit_body, headers=headers_dict)
     return response
-
-
-#  auth_token = get_new_user_token()
-
-
-#  kit_response = post_new_client_kit(data.kit_body, auth_token)
-#  print(kit_response.status_code)
-#  print(kit_response.text)
-
-
-#  def create_new_user(user_body):
-#    response = requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-#                             json=user_body,
-#                             headers=data.headers)
-#    return response.json()["authToken"]
-
-
-#  def post_new_client_kit(kit_body, auth_token):
-#    headers = {
-#        "Authorization": "Bearer " + auth_token,
-#        "Content-Type": "application/json",
-#    }
-#    url = configuration.URL_SERVICE + configuration.CREATE_KITS_PATH
-#    response = requests.post(url, json=kit_body, headers=headers)
-#    return response
-
-
-#  auth_token = create_new_user(data.user_body)
-
-#  kit_response = post_new_client_kit(data.kit_body, auth_token)
-#  print(kit_response.status_code)
-#  print(kit_response.text)
-
-
-#  def post_new_user(user_body):
-#      return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-#                           json=user_body,
-#                           headers=data.headers)
-
-
-#  response = post_new_user(data.user_body)
-#  print(response.status_code)
-#  print(response.text)
-#  print(response.json()['authToken'])
-
-
-#  def get_new_user_token():
-#    response = post_new_user(data.user_body)
-#    auth_token = response.json()['authToken']
-#    return response.json()['authToken']
-
-
-#  def post_new_client_kit(kit_body):
-#    headers = {
-#        "Authorization": "Bearer " + auth_token,
-#        "Content-Type": "application/json",
-#    }
-#    url = configuration.URL_SERVICE + configuration.CREATE_KITS_PATH
-#    response = requests.post(url, json=kit_body, headers=headers)
-#    return response
-
-
-#  auth_token = get_new_user_token()
-
-
-#  auth_token = create_new_user(data.user_body)
-
-
-#  kit_response = post_new_client_kit(data.kit_body)
-#  print(kit_response.status_code)
-#  print(kit_response.text)
